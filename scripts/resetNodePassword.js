102565430a81645af83bba753a9ac8c4f0e5ec13
dzotic9 <dzotic9@gmail.com>
2017-03-23 12:17:49 +0200

    Create resetNodePassword.js

---
+++ scripts/resetNodePassword.js
@@ -0,0 +1,53 @@
+import com.hivext.api.environment.Environment;
+
+var APPID = hivext.local.getParam("TARGET_APPID"),
+    sNewPassword = '${globals.password}',
+    nNodesLength,
+    oEnvNodes,
+    oResp,
+    i;
+
+oEnvService = hivext.local.exp.wrapRequest(new Environment(APPID, session));
+
+oEnvInfoResp = oEnvService.getEnvInfo();
+oEnvNodes = toNative(oEnvInfoResp).nodes;
+
+nNodesLength = oEnvNodes.length;
+
+for (i = 0; i < nNodesLength; i += 1) {
+    oResp = oEnvService.resetNodePasswordById({
+        nodeType: 'postgres9',
+        password: sNewPassword,
+        nodeId: oEnvNodes[i].id
+    });
+}
+    
+return {
+    result: 0
+};import com.hivext.api.environment.Environment;
+
+var APPID = hivext.local.getParam("TARGET_APPID"),
+    nNodesLength,
+    sNewPassword = '${globals.password}',
+    oEnvNodes,
+    oResp,
+    i;
+
+oEnvService = hivext.local.exp.wrapRequest(new Environment(APPID, session));
+
+oEnvInfoResp = oEnvService.getEnvInfo();
+oEnvNodes = toNative(oEnvInfoResp).nodes;
+
+nNodesLength = oEnvNodes.length;
+
+for (i = 0; i < nNodesLength; i += 1) {
+    oResp = oEnvService.resetNodePasswordById({
+        nodeType: 'postgres9',
+        password: sNewPassword,
+        nodeId: oEnvNodes[i].id
+    });
+}
+    
+return {
+    result: 0
+};
