4c1d1e3643bdb86fa7cee63ecf9e89df813bdf27
dzotic9 <dzotic9@gmail.com>
2017-06-09 12:21:54 +0300

    Update resetNodePassword.js

--- scripts/resetNodePassword.js
+++ scripts/resetNodePassword.js
@@ -24,30 +24,4 @@ for (i = 0; i < nNodesLength; i += 1) {
     
 return {
     result: 0
-};import com.hivext.api.environment.Environment;
-
-var APPID = hivext.local.getParam("TARGET_APPID"),
-    nNodesLength,
-    sNewPassword = '${globals.password}',
-    oEnvNodes,
-    oResp,
-    i;
-
-oEnvService = hivext.local.exp.wrapRequest(new Environment(APPID, session));
-
-oEnvInfoResp = oEnvService.getEnvInfo();
-oEnvNodes = toNative(oEnvInfoResp).nodes;
-
-nNodesLength = oEnvNodes.length;
-
-for (i = 0; i < nNodesLength; i += 1) {
-    oResp = oEnvService.resetNodePasswordById({
-        nodeType: 'postgres9',
-        password: sNewPassword,
-        nodeId: oEnvNodes[i].id
-    });
-}
-    
-return {
-    result: 0
 };
